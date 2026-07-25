26f789ffa68edc0889103c5afcb014b01eebaa50
sutocraft <309116562+sutocraft@users.noreply.github.com>
2026-07-25 23:00:07 +0600

    Update page.tsx

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,60 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { supabase } from "@/lib/supabase";
+
 export default function ProductsPage() {
-  return <h1>Products</h1>;
+  const [products, setProducts] = useState<any[]>([]);
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  async function loadProducts() {
+    const { data, error } = await supabase
+      .from("products")
+      .select(`
+        *,
+        categories(name)
+      `)
+      .order("created_at", { ascending: false });
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+
+    setProducts(data || []);
+  }
+
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <Link href="/admin/products/new">
+        Add Product
+      </Link>
+
+      <table border={1} cellPadding={10}>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {products.map((p) => (
+            <tr key={p.id}>
+              <td>{p.name}</td>
+              <td>{p.categories?.name}</td>
+              <td>{p.price}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
