875463ff46e1c67fed32fa3aceb3e0d63ac01f4c
sutocraft <309116562+sutocraft@users.noreply.github.com>
2026-07-28 21:54:25 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/website/Header";
 import Hero from "./components/website/Hero";
+import Categories from "./components/website/Categories";
 import FeaturedProducts from "./components/website/FeaturedProducts";
 
-export default function Home() {
+export default function HomePage() {
   return (
     <>
       <Header />
       <Hero />
+      <Categories />
       <FeaturedProducts />
     </>
   );
