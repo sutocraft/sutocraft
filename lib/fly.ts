87a2489bcd87c4cba95d30af3b4fab3543dc1377
sutocraft <309116562+sutocraft@users.noreply.github.com>
2026-08-08 00:13:19 +0600

    Update fly.ts

--- lib/fly.ts
+++ lib/fly.ts
@@ -1,34 +1,88 @@
 export function getElementCenter(
   element: HTMLElement
 ) {
-  const rect = element.getBoundingClientRect();
+  const rect =
+    element.getBoundingClientRect();
 
   return {
-    x: rect.left + rect.width / 2,
-    y: rect.top + rect.height / 2,
+    x:
+      rect.left +
+      rect.width / 2,
+
+    y:
+      rect.top +
+      rect.height / 2,
   };
 }
 
 export function getImageStartPosition(
   element: HTMLElement
 ) {
-  const rect = element.getBoundingClientRect();
+  const rect =
+    element.getBoundingClientRect();
 
   return {
-    x: rect.left + rect.width / 2,
-    y: rect.top + rect.height / 2,
+    x:
+      rect.left +
+      rect.width / 2,
+
+    y:
+      rect.top +
+      rect.height / 2,
   };
 }
 
 export function getCartPosition() {
-  const cart = document.getElementById("header-cart");
 
-  if (!cart) {
-    return {
-      x: window.innerWidth - 80,
-      y: 60,
-    };
+  const mobileBottom =
+    document.getElementById(
+      "bottom-cart"
+    );
+
+  if (
+    mobileBottom &&
+    window.innerWidth < 1024
+  ) {
+    return getElementCenter(
+      mobileBottom
+    );
   }
 
-  return getElementCenter(cart);
+  const mobileHeader =
+    document.getElementById(
+      "header-cart-mobile"
+    );
+
+  if (
+    mobileHeader &&
+    window.innerWidth < 1024
+  ) {
+    return getElementCenter(
+      mobileHeader
+    );
+  }
+
+  const desktop =
+    document.getElementById(
+      "header-cart"
+    );
+
+  if (desktop) {
+    return getElementCenter(
+      desktop
+    );
+  }
+
+  return {
+
+    x:
+      window.innerWidth - 50,
+
+    y:
+      window.innerWidth < 1024
+        ? window.innerHeight - 42
+        : 52,
+
+  };
+
 }
