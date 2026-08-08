8bcc9269be30005909867bbfbe6233bfbb5dbf22
sutocraft <309116562+sutocraft@users.noreply.github.com>
2026-08-08 23:07:09 +0600

    Update ProductsGrid.tsx

--- app/product/ProductsGrid.tsx
+++ app/product/ProductsGrid.tsx
@@ -19,11 +19,11 @@ export default function ProductsGrid({
       <div
         className="
           grid
-          grid-cols-2
-          gap-3
-          sm:gap-4
-          md:grid-cols-3
-          xl:grid-cols-4
+grid-cols-2
+gap-3
+sm:gap-4
+md:grid-cols-3
+xl:grid-cols-4
         "
       >
         {Array.from({ length: 8 }).map((_, index) => (
