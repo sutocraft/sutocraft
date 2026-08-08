7a77e2b8b6af3bb468949905602635c4e0632c9c
sutocraft <309116562+sutocraft@users.noreply.github.com>
2026-08-08 21:45:41 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-import { CartProvider } from "@/lib/cart-context";
-import CartDrawer from "@/app/components/website/CartDrawer";
-import {
-  ThemeProvider,
-} from "@/app/components/website/settings.theme_color";
-
-import CartFlyProvider from "@/app/components/website/CartFlyProvider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "SutoCraft",
@@ -36,26 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
+    <html lang="en" className="h-full antialiased">
       <body className="min-h-full flex flex-col">
-        <ThemeProvider>
-
-  <CartFlyProvider>
-
-    <CartProvider>
-
-      {children}
-
-      <CartDrawer />
-
-    </CartProvider>
-
-  </CartFlyProvider>
-
-</ThemeProvider>
+        {children}
       </body>
     </html>
   );
