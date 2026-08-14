3576fe5720f9d7d306c57d7bc096d9bde142f928
sutocraft <309116562+sutocraft@users.noreply.github.com>
2026-08-14 22:05:09 +0600

    Update page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -320,16 +320,11 @@ export default function CheckoutPage() {
    * PRODUCT IMAGE
    */
   function getImage(item: CartItem) {
-    return (
-      item.products?.image_url?.trim() ||
-      item.products?.product_images?.find(
-        (image) => image.is_primary
-      )?.image_url ||
-      item.products?.product_images?.[0]
-        ?.image_url ||
-      "/images/no-image.png"
-    );
-  }
+  return (
+    item.products?.image_url?.trim() ||
+    "/images/no-image.png"
+  );
+}
 
   /*
    * PLACE ORDER
